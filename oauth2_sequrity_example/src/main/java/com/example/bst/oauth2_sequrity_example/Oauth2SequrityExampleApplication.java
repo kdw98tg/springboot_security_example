02c169d34450dad7f2138eb24d979d0aa0666945
kdw98tg <117254224+kdw98tg@users.noreply.github.com>
2026-03-20 13:52:24 +0900

    project start

---
+++ oauth2_sequrity_example/src/main/java/com/example/bst/oauth2_sequrity_example/Oauth2SequrityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bst.oauth2_sequrity_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SequrityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SequrityExampleApplication.class, args);
+	}
+
+}
---
+++ oauth2_sequrity_example/src/test/java/com/example/bst/oauth2_sequrity_example/Oauth2SequrityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bst.oauth2_sequrity_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SequrityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
